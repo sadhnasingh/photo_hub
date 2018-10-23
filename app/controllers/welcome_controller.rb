ec8251a1af081cfcbca382463bcded0d0ff9242c
Arvind Kushwah <arvindkushwah9@gmail.com>
2018-10-23 18:43:23 +0530

    add gallery page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,7 @@ class WelcomeController < ApplicationController
   def index
   	@images = Image.all
   end
+  def gallery
+  	@images = Image.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  get 'gallery/index'
   get 'welcome/index'
   root 'welcome#index'
-  get 'gallery/index'
+  get 'welcome/gallery'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
